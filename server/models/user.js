beb61d4c5e231707a5300214c73876818150ffad
skalam02 <skalam02@gmail.com>
2018-02-04 16:01:22 -0500

    updates

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,50 @@
 var mongoose = require('mongoose')
+var validator = require('validator')
+var jwt = require('jsonwebtoken')
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: `{value} is not a valid email`
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [
+    {
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
+
+var User = mongoose.model('User', UserSchema)
+
+UserSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth'
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
+  
+  user.tokens.push({access, token})
+  
+  return user.save().then((token)=> {
+    return token;
+  })
+};
 
 module.exports = {
   User
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 var {ObjectID} = require('mongodb')
+var _ = require('lodash')
+
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
@@ -11,6 +13,8 @@ var port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
+
+//Get Requests
 app.get('/todos', (req,res) => {
   Todo.find().then((s)=>{
     res.send(s)
@@ -30,14 +34,18 @@ app.get('/todos/:id', isValid , (req,res) => {
   }).catch((e)=>{res.send.statusCode(400)})
 })
 
-app.delete('/todos/:id',isValid,(req,res) => {
-  var id = req.params.id
-  Todo.findByIdAndRemove(id).then((doc)=>{
-    res.send(doc)
-  }).catch((e)=>{
-    console.log(e)
-  })
-})
+
+//POST Requests
+app.post('/users', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  console.log(user)
+  
+  user.save()
+    .then((user)=>{return user.generateAuthToken()})
+    .then((token)=>{res.send(token)})
+    .catch((e)=> {res.status(400).send(e)})
+});
 
 app.post('/todos', (req,res) => {
   var todo = new Todo({
@@ -48,6 +56,38 @@ app.post('/todos', (req,res) => {
     res.send(doc)
   },(e)=> {
     res.status(400).send(e)
+  })
+})
+
+//PATCH Route
+app.patch('/todos/:id', isValid, (req,res) => {
+  var id = req.params.id
+  var body = _.pick(req.body, ['text','completed'])
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  
+  Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo) => {
+    if(!todo) {
+      res.status(404).send();
+    } else {
+      res.send(todo)
+    }
+  }).catch((e)=> {
+    res.status(400).send()
+  })
+})
+
+//DELETE Route
+app.delete('/todos/:id',isValid,(req,res) => {
+  var id = req.params.id
+  Todo.findByIdAndRemove(id).then((doc)=>{
+    res.send(doc)
+  }).catch((e)=>{
+    console.log(e)
   })
 })
 
